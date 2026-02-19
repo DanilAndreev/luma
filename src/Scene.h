4a791107e84ef068d98e8fea2525fa90d3b6ffd4
DanyloAndrieiev <danssg08@gmail.com>
2026-02-19 16:02:03 +0100

    Loader WIP

--- src/Scene.h
+++ src/Scene.h
@@ -2,9 +2,55 @@
 
 #include <DirectXMath.h>
 
+constexpr size_t VertexAttributesMaxTexCoords = 8;
+constexpr size_t VertexAttributesMaxColors = 8;
+
+enum class VertexAttributesMask : uint64_t {
+    None = 0x0, // Vertex has only float4 position per item
+    Normals = 0x1 << 0, // float3 normal vector
+    TexCoords0 = 0x1 << 1, // float2 texture coordinetes
+    TexCoords1 = 0x1 << 2, // float2 texture coordinetes
+    TexCoords2 = 0x1 << 3, // float2 texture coordinetes
+    TexCoords3 = 0x1 << 4, // float2 texture coordinetes
+    TexCoords4 = 0x1 << 5, // float2 texture coordinetes
+    TexCoords5 = 0x1 << 6, // float2 texture coordinetes
+    TexCoords6 = 0x1 << 7, // float2 texture coordinetes
+    TexCoords7 = 0x1 << 8, // float2 texture coordinetes
+    Color0 = 0x1 << 9, // float3 color
+    Color1 = 0x1 << 10, // float3 color
+    Color2 = 0x1 << 11, // float3 color
+    Color3 = 0x1 << 12, // float3 color
+    Color4 = 0x1 << 13, // float3 color
+    Color5 = 0x1 << 14, // float3 color
+    Color6 = 0x1 << 15, // float3 color
+    Color7 = 0x1 << 16, // float3 color
+};
+
+enum class VertexAttributesFlags : uint32_t {
+    HalfNormals = 0x1 << 0,
+    HalfTexCoords0 = 0x1 << 1,
+    HalfTexCoords1 = 0x1 << 2,
+    HalfTexCoords2 = 0x1 << 3,
+    HalfTexCoords3 = 0x1 << 4,
+    HalfTexCoords4 = 0x1 << 5,
+    HalfTexCoords5 = 0x1 << 6,
+    HalfTexCoords6 = 0x1 << 7,
+    HalfTexCoords7 = 0x1 << 8,
+    HalfColor0 = 0x1 << 9,
+    HalfColor1 = 0x1 << 10,
+    HalfColor2 = 0x1 << 11,
+    HalfColor3 = 0x1 << 12,
+    HalfColor4 = 0x1 << 13,
+    HalfColor5 = 0x1 << 14,
+    HalfColor6 = 0x1 << 15,
+    HalfColor7 = 0x1 << 16,
+};
+
 struct Mesh{
-    //TODO: maybe make vertex attributes more cache-local
-    std::vector<DirectX::XMFLOAT4> vertices;
+    VertexAttributesMask vaMask = VertexAttributesMask::None;
+    VertexAttributesFlags vaFlags = {};
+
+    std::vector<char> vertices;
     std::vector<uint32_t> indices;
     DirectX::XMFLOAT4X4 transform;
 };
--- src/SceneLoader.cpp
+++ src/SceneLoader.cpp
@@ -18,6 +18,92 @@ namespace Loader {
         }
     }
 
+	Mesh ModelLoader::processMesh(aiMesh* mesh, const aiScene* scene) {
+		// Data to fill
+		std::vector<VERTEX> vertices;
+		std::vector<UINT> indices;
+		std::vector<Texture> textures;
+
+		static_assert(std::is_same_v<decltype(mesh->mVertices.x), bool>);
+		
+		Mesh result{};
+
+		size_t vertexBufferStrideInBytes = sizeof(DirectX::XMFLOAT4);
+		if (mesh->HasNormals()) {
+			result.vaMask |= VertexAttributesMask::Normals;
+			vertexBufferStrideInBytes += sizeof(DirectX::XMFLOAT3);
+		}
+		for (size_t i = 0; i < VertexAttributesMaxTexCoords; ++i) {
+			if (mesh->HasTextureCoords(i)) {
+				result.vaMask |= VertexAttributesMask::TexCoords0 << i;
+				vertexBufferStrideInBytes += sizeof(DirectX::XMFLOAT2);
+			}
+		}
+		for (size_t i = 0; i < VertexAttributesMaxColors; ++i) {
+			if (mesh->HasVertexColors(i)) {
+				result.vaMask |= VertexAttributesMask::Color0 << i;
+				vertexBufferStrideInBytes += sizeof(DirectX::XMFLOAT4);
+			}
+		}
+		size_t vertexBufferSizeInBytes = mesh->mNumVertices * vertexBufferStrideInBytes;
+
+
+		result.vertices.resize(vertexBufferSizeInBytes);
+
+		char* vbCursor = result.vertices.data();
+		for (size_t vertexIdx = 0; vertexIdx < mesh->mNumVertices; ++vertexIdx) {
+			auto* position = reinterpret_cast<DirectX::XMFLOAT4*>(vbCursor);
+			position->x = mesh->mVertices[vertexIdx].x;
+			position->y = mesh->mVertices[vertexIdx].y;
+			position->z = mesh->mVertices[vertexIdx].z;
+			//TOOD: consider moving it to shader
+			position->w = 1.0f;
+			vbCursor += sizeof(*position);
+
+			if (mesh->HasNormals()) {
+				auto* normal = reinterpret_cast<DirectX::XMFLOAT3*>(vbCursor);
+				normal->x = mesh->mNormals[vertexIdx].x;
+				normal->y = mesh->mNormals[vertexIdx].y;
+				normal->z = mesh->mNormals[vertexIdx].z;
+				vbCursor += sizeof(*normal);
+			}
+			for (size_t i = 0; i < VertexAttributesMaxTexCoords; ++i) {
+				if (mesh->HasTextureCoords(i)) {
+					auto* texCoords = reinterpret_cast<DirectX::XMFLOAT2*>(vbCursor);
+					texCoords->x = mesh->mTextureCoords[i][vertexIdx].x;
+					texCoords->y = mesh->mTextureCoords[i][vertexIdx].y;
+					vbCursor += sizeof(*texCoords);
+				}
+			}
+			for (size_t i = 0; i < VertexAttributesMaxColors; ++i) {
+				if (mesh->HasVertexColors(i)) {
+					auto* color = reinterpret_cast<DirectX::XMFLOAT4*>(vbCursor);
+					color->x = mesh->mColors[i][vertexIdx].r;
+					color->y = mesh->mColors[i][vertexIdx].g;
+					color->z = mesh->mColors[i][vertexIdx].b;
+					color->w = mesh->mColors[i][vertexIdx].a;
+					vbCursor += sizeof(*color);
+				}
+			}
+		}
+
+		for (UINT i = 0; i < mesh->mNumFaces; i++) {
+			aiFace face = mesh->mFaces[i];
+
+			for (UINT j = 0; j < face.mNumIndices; j++)
+				indices.push_back(face.mIndices[j]);
+		}
+
+		if (mesh->mMaterialIndex >= 0) {
+			aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+
+			std::vector<Texture> diffuseMaps = this->loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse", scene);
+			textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+		}
+
+		return Mesh(dev_, vertices, indices, textures);
+	}
+
     bool LoadScene(Scene& scene) noexcept {
         Assimp::Importer importer;
 
