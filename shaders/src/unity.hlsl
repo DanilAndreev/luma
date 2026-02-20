4b9eea3948e7411124388875a6a800a1fe189d4d
DanyloAndrieiev <danssg08@gmail.com>
2026-02-20 16:16:35 +0100

    WIP

--- shaders/src/unity.hlsl
+++ shaders/src/unity.hlsl
@@ -1,20 +1,23 @@
-struct VSIn {
-    float4 position: POSITION;
-    float4 normal: NORMAL;
-    float4 texcoor0: TEXCOORD0;
-    float4 color0: COLOR0;
-}
+struct VSIn
+{
+    float4 position : POSITION;
+    float4 normal : NORMAL;
+    float4 texcoor0 : TEXCOORD0;
+    float4 color0 : COLOR0;
+};
 
-struct VSOut {
-    float4 position: POSITION;
-    float4 normal: NORMAL;
-    float4 texcoor0: TEXCOORD0;
-    float4 color0: COLOR0;
-}
+struct VSOut
+{
+    float4 position : POSITION;
+    float4 normal : NORMAL;
+    float4 texcoor0 : TEXCOORD0;
+    float4 color0 : COLOR0;
+};
 
-struct PSOut {
-    float4 color: COLOR0;
-}
+struct PSOut
+{
+    float4 color : COLOR0;
+};
 
 VSOut VSMain(VSIn input) {
     VSOut output{};
